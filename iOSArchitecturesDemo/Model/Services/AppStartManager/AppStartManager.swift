c98faedc9b0373a2727edccd8eb9ef7719ceafd5
Dmitry Dudenin <dmitry_dudenin@mail.ru>
2021-07-08 21:38:50 +0300

    Refactor TabBarController

--- iOSArchitecturesDemo/Model/Services/AppStartManager/AppStartManager.swift
+++ iOSArchitecturesDemo/Model/Services/AppStartManager/AppStartManager.swift
@@ -17,8 +17,23 @@ final class AppStartManager {
     }
     
     func start() {
-        let rootVC = NavigationMenuBaseController()
+        let softwearTab = UITabBarItem()
+        softwearTab.title = "Softwear"
+        softwearTab.image = UIImage(systemName: "app.fill")
+
+        let softwearVC = SearchAssembly.make()
+        softwearVC.tabBarItem = softwearTab
+        
+        let musicTab = UITabBarItem()
+        musicTab.title = "Music"
+        musicTab.image = UIImage(systemName: "music.note.list")
+
+        let musicVC = UIViewController()
+        musicVC.tabBarItem = musicTab
+        
+        let rootVC = UITabBarController()
         rootVC.navigationItem.title = "Search via iTunes"
+        rootVC.viewControllers = [softwearVC, musicVC]
         
         let navVC = self.configuredNavigationController
         navVC.viewControllers = [rootVC]
